a24cd352763296895d33516779908c45fbf90b21
baifumei <k_katerina_k@icloud.com>
2022-07-27 14:26:16 +0300

    Initial Commit

---
+++ Flag game/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Flag game
+//
+//  Created by Екатерина К on 27.07.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Flag game/Flag_gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Flag_gameApp.swift
+//  Flag game
+//
+//  Created by Екатерина К on 27.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct Flag_gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
