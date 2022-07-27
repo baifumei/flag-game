021f09990ad55ca7c98604d5df3550e3a4e2d20c
baifumei <k_katerina_k@icloud.com>
2022-07-27 21:53:59 +0300

    Three random flag images appear on screen

--- Flag game/ContentView.swift
+++ Flag game/ContentView.swift
@@ -9,14 +9,40 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var countries = ["UK", "USA", "Argentina", "Bangladesh", "Brazil", "Canada", "China", "Czech", "Denmark", "Finland", "Germany", "Greece", "Israel", "Japan", "Kazakhstan", "Russia", "Scotland", "SouthKorea", "Sweden", "Uzbekistan"].shuffled()
+    @State private var countries = ["UK", "USA", "Argentina", "Bangladesh", "Brazil", "Canada", "Czech", "Denmark", "Finland", "Germany", "Greece", "Japan", "Russia", "Scotland", "SouthKorea", "Sweden"].shuffled()
     @State private var correctAnswer = Int.random(in: 0...2)
     
     var body: some View {
         
         ZStack {
             LinearGradient(colors: [.black, .gray], startPoint: .top, endPoint: .bottom).edgesIgnoringSafeArea(.all)
+            VStack {
+                VStack {
+                    Text("Choose a flag:")
+                        .font(.headline)
+                        .fontWeight(.black)
+                        .foregroundColor(Color.white)
+                    
+                    Text(countries[correctAnswer])
+                        .font(.title)
+                        .fontWeight(.black)
+                        .foregroundColor(Color.white)
+                        .padding(.top, 1.0)
+                }
+                
+                ForEach(0..<3) { number in
+                    Button(action: {
+                        //
+                    }) {
+                        Image(self.countries[number])
+                            .frame(width: 250, height: 150)
+                            .clipShape(Rectangle())
+                            .shadow(color: .black, radius: 3)
+                    }
+                }
+            }
         }
+        
     }
 }
 
